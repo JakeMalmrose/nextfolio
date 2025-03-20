1d3b54fb15daf83b0555a7d38424753d27d4cce9
JakeMalmrose <jmalmrose@student.neumont.edu>
2025-03-20 08:36:03 -0600

    Removing some unnecessary options (default values)

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-  reactStrictMode: true,
   output: 'standalone', // Enable standalone output for better Docker support
-  swcMinify: true,
-  experimental: {
-    // Any experimental features you want to enable
-  },
 };
 
 export default nextConfig;
